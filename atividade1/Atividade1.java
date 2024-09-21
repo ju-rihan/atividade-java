d0108bd96705403360ba5cf8529d8da4b6b92ad7
Ryan <ryxn.lorran@gmail.com>
2024-09-20 21:48:12 -0300

    Update Atividade1.java

--- atividade1/Atividade1.java
+++ atividade1/Atividade1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.atividade1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -11,6 +13,30 @@ package com.mycompany.atividade1;
 public class Atividade1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Leitura dos dados
+        System.out.print("Informe o valor total da fatura: R$ ");
+        double valorTotal = scanner.nextDouble();
+
+        System.out.print("Informe o número de parcelas: ");
+        int numeroParcelas = scanner.nextInt();
+
+        System.out.print("Informe o percentual de desconto para pagamento antecipado: ");
+        double percentualDesconto = scanner.nextDouble();
+
+        System.out.print("Informe o percentual de multa por atraso: ");
+        double percentualMulta = scanner.nextDouble();
+
+        // Cálculo do valor total com desconto
+        double desconto = valorTotal * (percentualDesconto / 100);
+        double valorTotalComDesconto = valorTotal - desconto;
+
+        // Cálculo do valor de cada parcela
+        double valorParcela = valorTotalComDesconto / numeroParcelas;
+
+        // Cálculo da multa por atraso
+        double multaPorParcela = valorParcela * (percentualMulta / 100);
+        
     }
 }
